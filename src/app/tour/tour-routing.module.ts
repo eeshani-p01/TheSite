9771ef32efc6186c39523d7ff3f1ee8af1e7c892
Eeshani Patel <eeshani.p01@gmail.com>
2018-09-14 02:05:39 +0530

    Tours prototype done

--- src/app/tour/tour-routing.module.ts
+++ src/app/tour/tour-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TourComponent } from './tour.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TourComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
